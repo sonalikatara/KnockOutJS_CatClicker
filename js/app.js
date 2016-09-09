658241fdf73dee81803de3e30057e7d3394d6b7c
Sonali Shukla <sonali@Sonalis-MBP.attlocal.net>
2016-09-09 14:31:13 -0700

    create a simple one cat clicker

---
+++ js/app.js
@@ -0,0 +1,20 @@
+var catViewModel = function(){
+	        this.name = ko.observable("Pelope");
+            this.imageSrc = ko.observable("img/cat1.jpg"); 
+            this.clickCount = ko.observable(0);
+
+// increment click count each time a cat image is clicked
+            this.incrementCounter = function(){
+                 this.clickCount(this.clickCount()+1);
+            };
+//compute the level of the cat -- if click count > 100 the cat changes from New Born to Infant 
+            this.level = ko.computed(function(){
+            		if (this.clickCount() > 100) {
+            			return "Infant"; 
+            		} else {
+            			return "New Born";
+            		};
+            },this);
+};
+
+ko.applyBindings(new catViewModel());
